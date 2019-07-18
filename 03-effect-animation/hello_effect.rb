b3c18e5e93cda0dc8f16421707b073e7ceb3d55c
gabriel schiavo <gabriel@lewagon.org>
2019-07-18 10:27:38 -0300

    init current frame math

--- 03-effect-animation/hello_effect.rb
+++ 03-effect-animation/hello_effect.rb
@@ -20,52 +20,22 @@ class Explosion
 		@current_frame = 0
 	end
 
-
-end
-
-
-class GameWindow < Gosu::Window
-	# Add some screen size
-	def initialize(width=320, height=240, fullscreen=false)
-		super
-		@x = @y = 10
-		@draws = 0
-		@buttons_down = 0
-	end
-
 	def update
-		@x -=1 if button_down?(Gosu::KbLeft)
-		@x +=1 if button_down?(Gosu::KbRight)
-		@y -=1 if button_down?(Gosu::KbUp)
-		@y +=1 if button_down?(Gosu::KbDown)
-	end
-
-	def button_down(id)
-		close if id == Gosu::KbEscape
-		@buttons_down += 1
-	end
-
-	def button_up(id)
-		@buttons_down -= 1
-	end
-
-	def needs_redraw? # Returns true if:
-		@draws == 0 || @buttons_down > 0
+		@current_frame += 1 if frame_expired?
 	end
 
 	def draw
-		@draws += 1
-		# Display Message
-		# @message = Gosu::Image.from_text(
-		# self, 'Hello World!', Gosu.default_font_name, 30)
-		# Display Info
-		@message = Gosu::Image.from_text(
-		self, info, Gosu.default_font_name, 30)
-		@message.draw(@x, @y, 0)
+
 	end
+
+end
 
 	private
 
+	def current_frame
+		@animation[@current_frame % @animation.size]
+	end
+	
 	def info # Show information on UI
 		"[x:#{@x};y:#{@y}; draws: #{@draws}]"
 	end
